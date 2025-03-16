4dd337f3ade371da6327516e22cdc008eaaeb9dd
sakshuu <sakshisjadhav.120@gmail.com>
2025-03-16 18:50:30 +0530

    updated

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  // origin: 'http://localhost:3000',
   origin: 'https://portfolio-backend-4ogf.onrender.com',
   credentials: true,
 }));
